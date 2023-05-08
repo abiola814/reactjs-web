4dc7de23ddd988ab11fe05181930bd7cdc50e87e
abiola oladunjoye <abiola.oladunjoye@tm30.net>
2023-05-08 23:42:11 +0100

    origin

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 // const sockets = io('http://localhost:3001', { autoConnect: true, forceNew: true });
 
-const sockets = io('https://signalservers.onrender.com');
+const sockets = io('/');
 export default sockets;
